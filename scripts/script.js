d183277b569afe5b046e8e3157404399befb37b6
mysoulterious <ermolowaksenia@yandex.ru>
2020-12-07 23:02:20 +0400

    feat: add popup_create

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,64 @@
-const formElement = document.querySelector(".popup");
+const formEdit = document.querySelector(".popup_edit");
+
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
+
 const editButton = document.querySelector(".profile__edit-button");
-const closeButton = formElement.querySelector(".popup__close-button");
-const nameInput = formElement.querySelector(".input-text_type_name");
-const jobInput = formElement.querySelector(".input-text_type_job");
+const closeEditButton = formEdit.querySelector(".popup__close-button");
+
+const nameInput = formEdit.querySelector(".input-text_type_name");
+const jobInput = formEdit.querySelector(".input-text_type_job");
+
+const formAdd = document.querySelector(".popup_create");
+
+const addButton = document.querySelector(".profile__add-button");
+const closeAddButton = formAdd.querySelector(".popup__close-button");
+
+const places = document.querySelector(".places");
+const placesContainer = places.querySelector(".place");
 
 function formInsert() {
 	nameInput.value = profileTitle.textContent;
 	jobInput.value = profileSubtitle.textContent;
 }
 
-function openPopup() {
-	formElement.classList.add("popup_opened");
+function openPopup(popup) {
+	popup.classList.add("popup_opened");
 	formInsert();
 }
 
-function closePopup() {
-	formElement.classList.remove("popup_opened");
+function closePopup(popup) {
+	popup.classList.remove("popup_opened");
 }
 
-function formSubmitHandler(evt) {
+function formEditSubmit(evt) {
 	evt.preventDefault();
 	profileTitle.textContent = nameInput.value;
 	profileSubtitle.textContent = jobInput.value;
-	closePopup();
+	closePopup(formEdit);
 }
 
-editButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
-formElement.addEventListener("submit", formSubmitHandler);
+function formAddSubmit(evt) {
+	evt.preventDefault();
+	closePopup(formAdd);
+}
+/*
+function addPlace(name, link) {
+	const placeTemplate = document.querySelector("#place-template").content;
+	const placeElement = placeTemplate.cloneNode(true);
+	placeElement.querySelector('.place__title').textContent = name;
+  placeElement.querySelector('.place__image').textContent = link;
+	
+	placeElement.querySelector('.place__like-button').addEventListener('click', function (evt) {
+		evt.target.classList.toggle('place__like-button_active')
+}); 
+
+placesContainer.append(placeElement);
+}*/
+
+editButton.addEventListener("click", () => openPopup(formEdit));
+closeEditButton.addEventListener("click", () => closePopup(formEdit));
+formEdit.addEventListener("submit", formEditSubmit);
+addButton.addEventListener("click", () => openPopup(formAdd));
+closeAddButton.addEventListener("click", () => closePopup(formAdd));
+formAdd.addEventListener("submit", formAddSubmit);
