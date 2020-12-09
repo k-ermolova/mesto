197ca3fe00a6ea1c45b94071fe837e7c80da966a
mysoulterious <ermolowaksenia@yandex.ru>
2020-12-09 22:31:57 +0400

    fix: delete exposed code

--- scripts/script.js
+++ scripts/script.js
@@ -9,13 +9,17 @@ const closeEditButton = formEdit.querySelector(".popup__close-button");
 const nameInput = formEdit.querySelector(".input-text_type_name");
 const jobInput = formEdit.querySelector(".input-text_type_job");
 
+const placesContainer = document.querySelector(".places__list");
+const placeNameInput = placesContainer.querySelector(
+	".input-text_type_heading"
+);
+const placeLinkInput = placesContainer.querySelector(".input-text_type_link");
+const templateElement = document.querySelector(".place-template");
+
 const formAdd = document.querySelector(".popup_create");
 
 const addButton = document.querySelector(".profile__add-button");
 const closeAddButton = formAdd.querySelector(".popup__close-button");
-/*
-const places = document.querySelector(".places");
-const placesContainer = places.querySelector(".place"); */
 
 const initialCards = [
 	{
@@ -50,33 +54,6 @@ const initialCards = [
 	},
 ];
 
-function composeNewPlace(item) {
-		return `<li class="place">
-	<img
-		src="${item.link}"
-		alt=""
-		class="place__image"
-	/>
-	<div class="place__description">
-		<h3 class="place__title">${item.name}</h3>
-		<button
-			type="button"
-			class="place__like-button"
-			aria-label="Понравилось"
-		></button>
-	</div>
-</li>`;
-	}
-
-const placesContainer = document.querySelector(".places__list");
-function renderPlace() {
-	let newPlace = "";
-	newPlace = initialCards.map(composeNewPlace).join("");
-	placesContainer.insertAdjacentHTML("afterbegin", newPlace);
-}
-
-renderPlace();
-
 function formInsert() {
 	nameInput.value = profileTitle.textContent;
 	jobInput.value = profileSubtitle.textContent;
@@ -102,19 +79,6 @@ function formAddSubmit(evt) {
 	evt.preventDefault();
 	closePopup(formAdd);
 }
-/*
-function addPlace(name, link) {
-	const placeTemplate = document.querySelector("#place-template").content;
-	const placeElement = placeTemplate.cloneNode(true);
-	placeElement.querySelector('.place__title').textContent = name;
-  placeElement.querySelector('.place__image').textContent = link;
-	
-	placeElement.querySelector('.place__like-button').addEventListener('click', function (evt) {
-		evt.target.classList.toggle('place__like-button_active')
-}); 
-
-placesContainer.append(placeElement);
-}*/
 
 editButton.addEventListener("click", () => openPopup(formEdit));
 closeEditButton.addEventListener("click", () => closePopup(formEdit));
