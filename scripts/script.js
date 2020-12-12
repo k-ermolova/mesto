84b9953c8046c789293f95e3e9c2a20ea587da3c
mysoulterious <ermolowaksenia@yandex.ru>
2020-12-12 16:34:47 +0400

    style: delete comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,19 @@
-const formEdit = document.querySelector(".popup_edit"); //кнопка редактирования профиля
+const formEdit = document.querySelector(".popup_edit");
 const editButton = document.querySelector(".profile__edit-button");
 const closeEditButton = formEdit.querySelector(".popup__close-button");
 
-const profileTitle = document.querySelector(".profile__title"); //страница: имя и работа
+const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
-const nameInput = formEdit.querySelector(".input-text_type_name"); //форма: имя и работа
+const nameInput = formEdit.querySelector(".input-text_type_name");
 const jobInput = formEdit.querySelector(".input-text_type_job");
 
-const formAdd = document.querySelector(".popup_create"); //кнопка добавления нового места
+const formAdd = document.querySelector(".popup_create");
 const addButton = document.querySelector(".profile__add-button");
 const closeAddButton = formAdd.querySelector(".popup__close-button");
 
-const placesContainer = document.querySelector(".places__list"); //контейнер для новых мест на странице
-const placeNameInput = formAdd.querySelector(
-	//форма: название и ссылка на картинку
-	".input-text_type_heading"
-);
+const placesContainer = document.querySelector(".places__list");
+const placeNameInput = formAdd.querySelector(".input-text_type_heading");
 const placeLinkInput = formAdd.querySelector(".input-text_type_link");
 const templateElement = document.querySelector(".place-template");
 const removeButton = templateElement.querySelector(".place__delete-button");
@@ -25,7 +22,6 @@ const imagePopup = document.querySelector(".figure-popup");
 const closeImagePopup = imagePopup.querySelector(".popup__close-button");
 
 const initialPlaces = [
-	//список мест
 	{
 		name: "Архыз",
 		link:
@@ -59,32 +55,28 @@ const initialPlaces = [
 ];
 
 function formInsert() {
-	//присваиваем полям формы значения имени и работы со страницы
 	nameInput.value = profileTitle.textContent;
 	jobInput.value = profileSubtitle.textContent;
 }
 
 function openPopup(popup) {
-	//открываем форму
 	popup.classList.add("popup_opened");
-	formInsert(); //вызываем функцию, которая вносит значения со страницы в форму
+	formInsert();
 }
 
 function closePopup(popup) {
-	//закрываем форму
 	popup.classList.remove("popup_opened");
 	clearPopupInputs();
 }
 
 function formEditSubmit(evt) {
-	evt.preventDefault(); //сбрасываем несохраненные данные
-	profileTitle.textContent = nameInput.value; //делаем обратное присвоение данных страницы из формы
+	evt.preventDefault();
+	profileTitle.textContent = nameInput.value;
 	profileSubtitle.textContent = jobInput.value;
-	closePopup(formEdit); //и закрываем форму
+	closePopup(formEdit);
 }
 
 function composePlace({ name, link }) {
-	//собираем место
 	const placeElement = templateElement.content.cloneNode("true");
 	const nameElement = placeElement.querySelector(".place__title");
 	const linkElement = placeElement.querySelector(".place__image");
@@ -94,6 +86,7 @@ function composePlace({ name, link }) {
 	const likeButtonElement = placeElement.querySelector(".place__like-button");
 	nameElement.textContent = name;
 	linkElement.src = link;
+	
 	removeButtonElement.addEventListener("click", removePlace);
 	likeButtonElement.addEventListener("click", function (evt) {
 		evt.target.classList.toggle("place__like-button_active");
@@ -121,9 +114,9 @@ function addNewPlace() {
 }
 
 function formAddSubmit(evt) {
-	evt.preventDefault(); //сбрасываем несохраненные данные
+	evt.preventDefault();
 	addNewPlace();
-	closePopup(formAdd); //закрываем форму
+	closePopup(formAdd);
 	clearPopupInputs();
 }
 
@@ -142,10 +135,10 @@ function showImagePopup({ name, link }) {
 }
 
 renderPlacesList();
-editButton.addEventListener("click", () => openPopup(formEdit)); //*клик на кнопку редактирования - открой форму редактирования
-closeEditButton.addEventListener("click", () => closePopup(formEdit)); //клик на закрыть - закрой форму редактирования
-formEdit.addEventListener("submit", formEditSubmit); //клик на кнопку сохранить - сохрани новые пользовательские данные и закрой форму
-addButton.addEventListener("click", () => openPopup(formAdd)); //*клик на кнопку добавить - открой форму добавления
+editButton.addEventListener("click", () => openPopup(formEdit));
+closeEditButton.addEventListener("click", () => closePopup(formEdit));
+formEdit.addEventListener("submit", formEditSubmit);
+addButton.addEventListener("click", () => openPopup(formAdd));
 closeAddButton.addEventListener("click", () => closePopup(formAdd));
 formAdd.addEventListener("submit", formAddSubmit);
 closeImagePopup.addEventListener("click", () => closePopup(imagePopup));
